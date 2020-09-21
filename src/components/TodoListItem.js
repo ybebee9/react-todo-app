b114bf47c35a810320eba6cc6925a60f2b38a128
ybebee9 <ybebee9@gmail.com>
2020-09-22 00:33:24 +0900

    1차 최적화

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,10 +7,11 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo;
     return (
-        <div className="TodoListItem">
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
             <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
                 {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank />}
                 <div className="text">{ text }</div>
@@ -19,7 +20,8 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
                 <MdRemoveCircleOutline />
             </div>
         </div>
+        </div>
     );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
